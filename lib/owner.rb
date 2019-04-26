eee29ef7270a96100fb3900e06d12d35579e6117
proactive-hashmap-4080 <kimpham54@gmail.com>
2019-04-26 23:56:57 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,60 @@
 class Owner
-  # code goes here
+
+
+  def self.all
+end
+
+def self.count
+end
+
+def self.reset_all
+end
+
+def species
+end
+
+def say_species
+end
+
+
+def name
+end
+
+def pets
+end
+
+
+def buy_fish
+end
+
+
+def buy_cat
+end
+
+def buy_dog
+
+end
+
+
+def walk_dogs
+end
+
+def play_with_cats
+
+end
+
+def feed_fish
+end
+
+def sell_pets
+end
+
+def list_pets
+end
+
+
+
+
+
+
 end
