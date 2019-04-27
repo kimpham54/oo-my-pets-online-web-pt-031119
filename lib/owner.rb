07d965667cfb9bed8222753e722e774a40654697
proactive-hashmap-4080 <kimpham54@gmail.com>
2019-04-27 00:57:54 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -78,6 +78,7 @@ def sell_pets
   petmood(:dogs,"nervous")
   petmood(:cats,"nervous")
   petmood(:fishes,"nervous")
+  owner.pets.clear
   # pets[:fishes].map do |fish|
   #   fish.mood = "nervous"
   # end
